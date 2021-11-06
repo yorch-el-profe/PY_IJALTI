a93bb87754f620acb802acca5a51e318e9ff6404
Jorge Ramón <jorge.ramon2128@gmail.com>
2021-11-06 01:57:22 -0600

    Ejemplos de ejercicios con funciones

---
+++ sesion03/02_funcion.py
@@ -0,0 +1,28 @@
+"""
+Función
+
+Es un bloque de código que puede recibir parámetros y puede regresar algún valor.
+
+Tanto los parámetros como el valor de retorno en una función son OPCIONALES!!
+
+El uso más común de una función es la reutilización de código.
+
+Sintaxis:
+
+def nombre_de_funcion(parametros):
+  bloque de codigo
+  linea1
+  linea2
+  linea3
+"""
+
+# Definiendo a la función
+def mi_primer_funcion():
+  print("Hola Mundo desde una función :)")
+
+# print(mi_primer_funcion)
+
+
+# Ejecutar la función (invocar, utilizarla, llamarla)
+# Sintaxis: nombre_funcion(parametros)
+mi_primer_funcion()
---
+++ sesion03/03_ejemplos_funciones.py
@@ -0,0 +1,73 @@
+"""
+1. Crear una función que reciba como parámetro el nombre de una persona y lo salude de la siguiente forma:
+
+"Hola, [nombre de la persona]"
+"""
+
+"""
+Los parámetros de una función son como variables que se pueden acceder únicamente dentro de la función.
+"""
+def saluda(nombre):
+  print("Hola, " + nombre)
+
+#saluda("Enrique")
+#saluda("Maria")
+#saluda("Gabriel")
+
+"""
+2. Crear una función que regrese la suma de dos números. 
+Suposición: los dos números son números enteros.
+"""
+
+"""
+"return" va a regresar un valor fuera de la función y finaliza la ejecución de la misma.
+
+Las funciones sólo pueden regresar 1 valor a la vez.
+"""
+def suma(numero1, numero2):
+  return numero1 + numero2
+
+#print(suma(1, 2))
+#print(suma(-1, 0))
+#print(suma(3.1416, 5))
+
+"""
+3. Crear una función que convierta los minutos en segundos
+
+3 -> 3 * 60 = 180
+"""
+def minutos_a_segundos(minutos):
+  return minutos * 60
+
+#print(minutos_a_segundos(3))
+#print(minutos_a_segundos(60))
+
+"""
+4. Crear una función que reciba una cantidad INDETERMINADA DE NÚMEROS
+y regrese la suma de todos
+"""
+
+# Versión chafa
+# Aceptar como parámetro una lista
+def sumaInfinitaLista(lista):
+  total = 0
+
+  for numero in lista:
+    total += numero
+
+  return total
+
+
+# print(sumaInfinitaLista([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 20, 30, 40, 50]))
+
+# Versión buena
+# * marca una cantidad indeterminada de parámetros
+def sumaInfinita(*numeros):
+  total = 0
+
+  for numero in numeros:
+    total += numero
+
+  return total
+
+print(sumaInfinita(100, 40, 10, 22, 2, 3, 2, 12, 3, 5, 5, 6, 3, 2, 234))
