13a00e343903af10626e83cc63ac625fe7f1ee08
Jorge Ramón <jorge.ramon2128@gmail.com>
2021-11-20 09:30:35 -0600

    Agregando ejemplo de herencia

--- sesion04/07_herencia.py
+++ sesion04/07_herencia.py
@@ -48,4 +48,27 @@ class Papitas(Producto):
     self.sabor = sabor
     self.tamaño = tamaño
 
+class Refresco(Producto):
+
+  def __init__(self, nombre, precio, sellos, sabor, presentacion):
+      super().__init__(nombre, precio, sellos)
+      self.sabor = sabor
+      self.presentacion = presentacion
+
+class BebidaAlcoholica(Producto):
+
+  def __init__(self, nombre, precio, sellos, gradosAlcohol):
+      super().__init__(nombre, precio, sellos)
+      self.gradosAlcohol = gradosAlcohol
+
+class Cerveza(BebidaAlcoholica):
+
+  def __init__(self, nombre, precio, sellos, gradosAlcohol, esClara):
+      super().__init__(nombre, precio, sellos, gradosAlcohol)
+      self.esClara = esClara
+
 sabritas = Papitas("Sabritas", 15, 2, "Naturales", "Bolzaza")
+
+coca = Refresco("Coca Cola", 15, 3, "Cola", "600ml")
+
+indio = Cerveza("Indio", 13, 0, 4, False)
